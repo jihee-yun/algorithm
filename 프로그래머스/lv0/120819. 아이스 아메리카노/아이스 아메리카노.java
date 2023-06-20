99483c60d7cfa2700828bbde2540a2e61b403c1c
jihee-yun <121940386+jihee-yun@users.noreply.github.com>
2023-06-20 19:14:41 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,15 +1,8 @@
 class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
-      if (money >= 5500) {
-            int i = money / 5500;
-            int rest = money % (5500 * i);
-            answer[0] = i;
-            answer[1] = rest;
-        } else {
-            answer[0] = 0;
-            answer[1] = money;
-        }
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
         return answer;
     }
 }
