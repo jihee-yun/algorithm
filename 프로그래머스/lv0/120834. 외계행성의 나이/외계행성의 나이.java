1e8c6ccc459090cd67287d70f4b9bbbf03824c18
윤지희 <121940386+jihee-yun@users.noreply.github.com>
2023-08-27 23:42:08 +0900

    [level 0] Title: 외계행성의 나이, Time: 1.30 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -2,14 +2,11 @@ class Solution {
     public String solution(int age) {
         String answer = "";
         String[] arr = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-        if(age > 9 && age < 100) {
-            answer = arr[age / 10] + arr[age % 10];
+        
+        while(age > 0) {
+            answer = arr[age % 10] + answer;
+            age /= 10;
         }
-        else if(100 <= age && age < 1000) {
-            answer = arr[(age / 10) / 10] + arr[(age / 10) % 10] + arr[age % 10];
-        } else if(age == 1000) {
-            answer = arr[((age / 10) / 10) / 10] + arr[((age / 10) / 10) % 10] + arr[(age / 10) % 10] + arr[age % 10];
-        } else if(age < 10) answer = arr[age % 10];
         
         return answer;
     }
