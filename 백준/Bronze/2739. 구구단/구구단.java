0a1a9c85a38d68efa65b8f6f349753cd610d013b
윤지희 <121940386+jihee-yun@users.noreply.github.com>
2023-12-10 21:36:05 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+           for(int j = 1; j <= 9; j++) {
+               System.out.println(n + " * " + j + " = " + n * j);
+        }
+    }
+}
